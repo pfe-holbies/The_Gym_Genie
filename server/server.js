ed27cd946ea02d7f08c50763922e2aa70bb40369
MounaBenAli <benalimouna1986@gmail.com>
2023-04-10 10:49:48 +0200

    Entry Point for running the server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const app = require('./index');
 const colors = require('colors');
 require('dotenv').config();
 
+
 // Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`🚀 Server is up and running on port ${port}/graphql`.magenta.bold));
+
