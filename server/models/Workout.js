20ba0867b0682259ab8b7a8a943342b89e0c608f
Mouna Ben Ali <90987270+MounaBenAli@users.noreply.github.com>
2023-04-13 07:07:33 +0100

    Update Workout.js
    change to follow JavaScript naming conventions

--- server/models/Workout.js
+++ server/models/Workout.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const workoutSchema = new mongoose.Schema(
+const WorkoutSchema = new mongoose.Schema(
     {
       name: {
         type: String,
@@ -15,6 +15,4 @@ const workoutSchema = new mongoose.Schema(
     },
   );
   
-  const Workout = mongoose.model('Workout', workoutSchema);
-  
-  module.exports = Workout; 
+ module.exports = mongoose.model('Workout', WorkoutSchema);
