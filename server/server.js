a88bc59877a907e18780bb770371d2ff2070ff3d
saifelleuch <sa2014if@gmail.com>
2023-04-26 14:41:11 +0100

    last time app config

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
+/* global process */
 const app = require("./app");
-const colors = require("colors");
+const colors = require("colors"); // eslint-disable-line no-unused-vars
 const dotenv = require("dotenv");
 
 // Load environment variables
 dotenv.config();
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Start the server
 app.listen(port, () =>
